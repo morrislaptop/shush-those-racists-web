8ebf53665796affc80afd194388373acb1fa3c5a
Craig Morris <craig.michael.morris@gmail.com>
2018-08-19 10:56:56 +0100

    envify

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
   <div id="app" class="p-8">
     <div class="flex justify-between">
       <div class="flex">
-        <a href="#" class="border-2 border-red-orange rounded-full p-4 w-8 h-8 text-white center-center mr-5"><i class="fab fa-twitter"></i></a>
-        <a href="#" class="border-2 border-red-orange rounded-full p-4 w-8 h-8 text-white center-center mr-5"><i class="fab fa-facebook-f"></i></a>
-        <a href="#" class="border-2 border-red-orange rounded-full p-4 w-8 h-8 text-white center-center"><i class="far fa-envelope"></i></a>
+        <a :href="twitterUrl" class="border-2 border-red-orange rounded-full p-4 w-8 h-8 text-white center-center mr-5"><i class="fab fa-twitter"></i></a>
+        <a :href="facebookUrl" class="border-2 border-red-orange rounded-full p-4 w-8 h-8 text-white center-center mr-5"><i class="fab fa-facebook-f"></i></a>
+        <a :href="emailUrl" class="border-2 border-red-orange rounded-full p-4 w-8 h-8 text-white center-center"><i class="far fa-envelope"></i></a>
       </div>
 
-      <a href="#" class="uppercase text-white border-2 border-red-orange p-4 tracking-wide font-black">Donate to the Goodies</a>
+      <a :href="donateUrl" class="uppercase text-white border-2 border-red-orange p-4 tracking-wide font-black">Donate to the Goodies</a>
     </div>
 
     <div class="text-center max-w-md mx-auto">
@@ -15,22 +15,33 @@
       <p class="mb-4 font-lato text-center tracking-wide font-bold text-white">Tired of racist Pollies like Bob Katter, Fraser Anning and Pauline Hanson making noise and filling up your feed?  Now you can shush those racists!</p>
       <p class="mb-8 font-lato text-center tracking-wide font-bold text-white">Shush Those Racists is a browser extension that replaces the names of our racist Politicians with colourful one liners, created by regular, non-racist Australians like you.</p>
       <p class="mb-8 text-2xl font-heavy text-white">Let’s call Bob Katter “that drunk racist uncle who hates Chinese people but eats the Chinese leftovers you were saving in the fridge”</p>
-      <a href="#" class="inline-block uppercase text-red-orange p-4 tracking-wide font-black bg-white">Install in Chrome</a>
+      <a :href="extUrl" class="inline-block uppercase text-red-orange p-4 tracking-wide font-black bg-white">Install in Chrome</a>
     </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  computed: {
+    facebookUrl () {
+      return `https://www.facebook.com/sharer.php?u=${process.env.VUE_APP_SHARE_URL}` 
+    },
+
+    twitterUrl () {
+      return `https://twitter.com/intent/tweet?url=${process.env.VUE_APP_SHARE_URL}`
+    },
+
+    emailUrl () {
+      return `mailto:?subject=${process.env.VUE_APP_SHARE_URL}`
+    },
+
+    extUrl () {
+      return process.env.VUE_APP_EXT_URL
+    },
+
+    donateUrl () {
+      return process.env.VUE_APP_DONATE_URL
+    },
   }
 }
 </script>
-
-<style>
-
-</style>
